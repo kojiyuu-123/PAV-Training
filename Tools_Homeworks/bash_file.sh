13eed6bf3607813ca97ef88a4195353faa741337
kojiyuu-123 <yusuke.kojima@pascalia.co.jp>
2020-04-06 14:46:26 +0900

    Tools of web-development

--- Tools_Homeworks/bash_file.sh
+++
@@ -1,4 +0,0 @@
-#Set a bash file's execute permission
-chmod 777 "bash_file.sh"
-
-echo "Hello,World!"
--- Tools_Homeworks/bash_file_3.sh
+++
@@ -1,6 +0,0 @@
-chmod 777 "bash_file_3.sh"
-
-#search the word,"character" in test.txt
-
-grep --color=auto character tmp/test.txt
-
