f97f398c337acd7e747f5803b917968b36659955
kojiyuu-123 <yusuke.kojima@pascalia.co.jp>
2020-04-09 09:08:34 +0900

    Fix this file ,bash_file_5.sh

--- Tools_webdevelopment_Homework/bash_file_5.sh
+++ Tools_webdevelopment_Homework/bash_file_5.sh
@@ -1,11 +1,22 @@
 chmod 777 "bash_file_5.sh"
 
-array=(bash_file/bash_file.sh bash_file_2/bash_file_2.sh bash_file_3/bash_file_3.sh bash_file_4/bash_file_4.sh)
+# clone repositories
+url_array=(https://github.com/kojiyuu-123/bash_file.git https://github.com/kojiyuu-123/bash_file_2.git https://github.com/kojiyuu-123/bash_file_3.git https://github.com/kojiyuu-123/bash_file_4.git)
 
-for i in ${array[@]}
+for i in ${url_array[@]};
 do
-	./$i
+	git clone ${i}
+done
+
+# fetch all repositories
+array=(bash_file bash_file_2 bash_file_3 bash_file_4)
+for j in ${array[@]};
+do
+	cd ${j}
+	git checkout master
+	git pull origin master
+	cd ../
+	./${j}/bash_file*.sh
 	echo -e ""
 done
 
-git fetch origin
